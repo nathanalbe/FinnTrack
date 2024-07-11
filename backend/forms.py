225927ee441f4ddcbc565104aaf6a34c7c8684da
timalsinab <timalsinabishal7@gmail.com>
2024-07-11 12:26:55 -0400

    Added financial plan

--- backend/forms.py
+++ backend/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, FloatField, IntegerField, DateField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField,DecimalField,DateField, PasswordField, SubmitField, BooleanField, FloatField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, NumberRange
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -34,15 +34,21 @@ class UpdateSpendingForm(FlaskForm):
     description = StringField('Description', validators=[DataRequired()])
     amount = FloatField('Amount', validators=[DataRequired()])
     submit = SubmitField('Update Spending')
+
+class FinancialGoalForm(FlaskForm):
+    name = StringField('Goal Name', validators=[DataRequired()])
+    target_amount = DecimalField('Target Amount', validators=[DataRequired(), NumberRange(min=0)])
+    current_amount = DecimalField('Current Amount', validators=[DataRequired(), NumberRange(min=0)], default=0.0)
+    due_date = DateField('Due Date', validators=[DataRequired()])
+    submit = SubmitField('Add Goal')
+
+class UpdateFinancialGoalForm(FlaskForm):
+    name = StringField('Goal Name', validators=[DataRequired()])
+    target_amount = DecimalField('Target Amount', validators=[DataRequired(), NumberRange(min=0)])
+    current_amount = DecimalField('Current Amount', validators=[DataRequired(), NumberRange(min=0)])
+    due_date = DateField('Due Date', validators=[DataRequired()])
+    submit = SubmitField('Update Goal')
     
 class StockSearchForm(FlaskForm):
     symbol = StringField('Stock Symbol', validators=[DataRequired(), Length(min=1, max=10)])
     submit = SubmitField('Search')
-
-class InvestmentForm(FlaskForm):
-    symbol = StringField('Stock Symbol', validators=[DataRequired()])
-    quantity = IntegerField('Quantity', validators=[DataRequired()])
-    purchase_price = FloatField('Purchase Price', validators=[DataRequired()])
-    purchase_date = DateField('Purchase Date', validators=[DataRequired()])
-    submit = SubmitField('Add Investment')
-
